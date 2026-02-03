39960179484d338d31dfe2831640d5261b996562
Camden MacIver <camdenmac@icloud.com>
2026-02-03 16:18:11 -0500

    Installed dialogue manager and translated the entire story into it win main_narrative.tscn

--- comp-vis-narrative/globals.gd
+++ comp-vis-narrative/globals.gd
@@ -1,7 +1,13 @@
-class_name Globals
 extends Node
 
 ## You put all of the state in here
 
 # access anywhere via Globals.number (i think)
 @export var number = 0 
+
+
+## INTERACTIVE NARRATIVE STATE
+@export var time = ""
+@export var prey = ""
+@export var POV = ""
+@export var nano_hurt_head : bool = false
---
+++ comp-vis-narrative/interactive narrative/gameplay/main_narrative.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Globals.time = "morning" # having default values for these is probably only a good thing
+	Globals.prey = "turtle"
+	Globals.POV = "NanoTyrannus"
+	Globals.nano_hurt_head = false
+	
+	DialogueManager.show_dialogue_balloon_scene(
+		load("res://interactive narrative/gameplay/balloon.tscn"), 
+		load("res://interactive narrative/gameplay/test_dialogue.dialogue"),
+		"start"
+		#"ending_3" # change back to "start" later
+	)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func update_game_time():
+	print("this works, you can just name any function in this script and it just does it")
+	if (Globals.time_of_day == "morning"):
+		pass
+		# TODO
+	
+	if (Globals.time_of_day == "noon"):
+		pass
+		# TODO
+	
+	if (Globals.time_of_day == "night"):
+		pass
+		# TODO
+
+
+func render_prey():
+	if (Globals.prey == "alligator"):
+		pass
+		# TODO
+	
+	if (Globals.time_of_day == "turtle"):
+		pass
+		# TODO
