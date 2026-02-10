3744419edc34f2eb6ce3c835cb3c34745ba499ca
Camden MacIver <camdenmac@icloud.com>
2026-02-09 22:22:13 -0500

    stupid change

--- comp-vis-narrative/interactive narrative/gameplay/main_narrative.gd
+++ comp-vis-narrative/interactive narrative/gameplay/main_narrative.gd
@@ -18,8 +18,8 @@ func _ready() -> void:
 	DialogueManager.show_dialogue_balloon_scene(
 		load("res://interactive narrative/gameplay/balloon.tscn"), 
 		load("res://interactive narrative/gameplay/main.dialogue"),
-		#"start"
-		"environment"
+		"start"
+		#"environment"
 		#"ending_2"
 		#"ending_3" # change back to "start" later
 	)
