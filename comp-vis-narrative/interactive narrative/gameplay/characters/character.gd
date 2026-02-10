1d9193f337b13a7985daf67e41ab74542736a9f8
Camden MacIver <camdenmac@icloud.com>
2026-02-09 20:18:23 -0500

    color adjust thing for characters

--- comp-vis-narrative/interactive narrative/gameplay/characters/character.gd
+++ comp-vis-narrative/interactive narrative/gameplay/characters/character.gd
@@ -5,11 +5,18 @@ class_name Character
 func _ready() -> void:
 	modulate = Color(1, 1, 1, 0)
 	
+	var target_color = Color("fff")
+	if Globals.time == "morning":
+		target_color = Color("faa084")
+	
+	if Globals.time == "night":
+		target_color = Color("9999bc")
+	
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "modulate", Color(1, 1, 1, 1), 0.5)
+	tween.tween_property(self, "modulate", target_color, 0.5)
 
 # Fades the the thing out and queue frees it
 func fade_out():
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "modulate", Color(1, 1, 1, 0), 0.5)
+	tween.tween_property(self, "modulate", Color.TRANSPARENT, 0.5)
 	tween.tween_callback(self.queue_free)
