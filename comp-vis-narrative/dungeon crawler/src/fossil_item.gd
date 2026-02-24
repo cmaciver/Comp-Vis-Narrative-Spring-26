702524e9216a8388bdf78bbed8c79857584e190c
cmaguiar <aguiar.claire@gmail.com>
2026-02-24 16:30:14 -0500

    updated fossil item
    fixing some physics weirdness

--- comp-vis-narrative/dungeon crawler/src/fossil_item.gd
+++ comp-vis-narrative/dungeon crawler/src/fossil_item.gd
@@ -2,9 +2,9 @@ extends RigidBody3D
 
 var isInteractable = true
 var interactableText = "Press \"e\" to pick up"
+@onready var player = get_tree().root.get_node("Node3D/Player")
 
 func interact():
-	var player = get_tree().root.get_node("Node3D/Player")
 	if player != null:
 		reparent(player)
 		gravity_scale = 0.0
@@ -21,3 +21,5 @@ func drop():
 	isInteractable = true
 	freeze = false
 	collision_layer = 1
+	position -= player.global_transform.basis.z * 1.25
+	
