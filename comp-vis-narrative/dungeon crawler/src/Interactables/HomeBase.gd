f235e79a95a3b1a0289ed62f8922e9d66cf1bfe0
ErikSvanes <116416661+ErikSvanes@users.noreply.github.com>
2026-02-26 12:19:40 -0500

    home-base

---
+++ comp-vis-narrative/dungeon crawler/src/Interactables/HomeBase.gd
@@ -0,0 +1,24 @@
+extends Area3D
+
+@export var victory_label: Label  # assign in Inspector
+
+func _ready():
+	body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(body):
+	print("intersection :)")
+	if body.is_in_group("fossil"):
+		print("intersection with fossil!")
+		if body.is_holding != null:
+			body.is_holding.queue_free()
+			body.is_holding = null
+			show_victory_message()
+
+func show_victory_message():
+	if victory_label:
+		victory_label.text = "🦕 Fossil secured! Great find, paleontologist!"
+		victory_label.visible = true
+		# Hide it after 3 seconds
+		await get_tree().create_timer(3.0).timeout
+		victory_label.visible = false
+		
