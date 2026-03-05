246505f09f7d267ed6a5d038d446347d016e227a
Camden MacIver <camdenmac@icloud.com>
2026-03-05 12:14:17 -0500

    removed the print god help us all

--- comp-vis-narrative/dungeon crawler/src/Godot-Simple-First-Person-Controller-main/player/player.gd
+++ comp-vis-narrative/dungeon crawler/src/Godot-Simple-First-Person-Controller-main/player/player.gd
@@ -204,7 +204,8 @@ func _physics_process(delta: float) -> void:
 		elif wants_sprint and can_sprint and stamina > 0.0:
 			target_speed = sprint_speed
 			do_sprint = true
-			print("Sprinting. Current stamina: " + str(stamina) + ", Current total weight: " + str(inventory.get_total_weight()))
+			# dear god
+			#print("Sprinting. Current stamina: " + str(stamina) + ", Current total weight: " + str(inventory.get_total_weight()))
 		else:
 			do_sprint = false
 
