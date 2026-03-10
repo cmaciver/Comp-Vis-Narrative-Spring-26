48aa6b0a27f63233014f77132ed181952c2c83a0
Camden MacIver <camdenmac@icloud.com>
2026-03-10 14:15:09 -0400

    deleted a lot and changed sky to a procedural sky

--- comp-vis-narrative/dungeon crawler/src/cloud_scroll.gd
+++ comp-vis-narrative/dungeon crawler/src/cloud_scroll.gd
@@ -1,13 +1,13 @@
 extends WorldEnvironment
 
-var cloud_noise
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	cloud_noise = environment.sky.sky_material.get("shader_parameter/cloud_shape_sampler")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	await get_tree().create_timer(0.01).timeout
-	cloud_noise.noise.offset.x += 0.001
+#var cloud_noise
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#cloud_noise = environment.sky.sky_material.get("shader_parameter/cloud_shape_sampler")
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+	#await get_tree().create_timer(0.01).timeout
+	#cloud_noise.noise.offset.x += 0.001
